e826d1da6220ad4f9104c3aa7da59dd2e9cc4db7
jacktembo <jack@jacktembo.com>
2024-06-21 15:31:13 +0200

    ...

--- blog/xmlrpc0db0.php
+++ blog/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://www.devmindgroup.com/blog</homePageLink>
+		<homePageLink>https://www.devmindgroup.tech/blog</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www.devmindgroup.com/blog/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.devmindgroup.com/blog/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.devmindgroup.com/blog/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www.devmindgroup.com/blog/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.devmindgroup.com/blog/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://www.devmindgroup.tech/blog/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.devmindgroup.tech/blog/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.devmindgroup.tech/blog/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://www.devmindgroup.tech/blog/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.devmindgroup.tech/blog/wp-json/" />
 			</apis>
 	</service>
 </rsd>
